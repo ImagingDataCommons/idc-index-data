8ec2afe2dd7ab98ed2368f4a37d12a9b6ad1402b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-03-11 11:42:57 -0400

    feat: Distribute "idc_index.csv.zip" within the wheel
    Switch the build-backend from "hatchling" to "scikit-build-core" to leverage
    CMake as scripting language while building a pure Python wheel.
    
    Disable submission of coverage reports as the project is not explicitly registered.

--- src/idc_index_data/__init__.py
+++ src/idc_index_data/__init__.py
@@ -6,6 +6,28 @@ idc-index-data: ImagingDataCommons index to query and download data.
 
 from __future__ import annotations
 
+import sys
+from pathlib import Path
+
+if sys.version_info >= (3, 10):
+    from importlib.metadata import distribution
+else:
+    from importlib_metadata import distribution
+
 from ._version import version as __version__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "IDC_INDEX_CSV_ARCHIVE_FILEPATH"]
+
+
+def _lookup(path: str) -> Path:
+    """Support editable installation by looking up path using distribution API."""
+    files = distribution("idc_index_data").files
+    if files is not None:
+        for _file in files:
+            if str(_file) == path:
+                return Path(str(_file.locate())).resolve(strict=True)
+    msg = f"Failed to lookup '{path}`."
+    raise FileNotFoundError(msg)
+
+
+IDC_INDEX_CSV_ARCHIVE_FILEPATH: Path = _lookup("idc_index_data/idc_index.csv.zip")
--- tests/test_package.py
+++ tests/test_package.py
@@ -7,3 +7,8 @@ import idc_index_data as m
 
 def test_version():
     assert importlib.metadata.version("idc_index_data") == m.__version__
+
+
+def test_filepath():
+    assert m.IDC_INDEX_CSV_ARCHIVE_FILEPATH.is_file()
+    assert m.IDC_INDEX_CSV_ARCHIVE_FILEPATH.name == "idc_index.csv.zip"
