c196bb18dac7e09aff7e7e06a94a47b39392b82b
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-03-22 19:20:22 -0400

    feat: Add IDC_INDEX_DATA_GENERATE_PARQUET option

--- src/idc_index_data/__init__.py
+++ src/idc_index_data/__init__.py
@@ -23,16 +23,23 @@ __all__ = [
 ]
 
 
-def _lookup(path: str) -> Path:
+def _lookup(path: str, optional: bool = False) -> Path | None:
     """Support editable installation by looking up path using distribution API."""
     files = distribution("idc_index_data").files
     if files is not None:
         for _file in files:
             if str(_file) == path:
                 return Path(str(_file.locate())).resolve(strict=True)
+    if optional:
+        return None
+
     msg = f"Failed to lookup '{path}`."
     raise FileNotFoundError(msg)
 
 
-IDC_INDEX_CSV_ARCHIVE_FILEPATH: Path = _lookup("idc_index_data/idc_index.csv.zip")
-# IDC_INDEX_PARQUET_FILEPATH: Path = _lookup("idc_index_data/idc_index.parquet")
+IDC_INDEX_CSV_ARCHIVE_FILEPATH: Path | None = _lookup(
+    "idc_index_data/idc_index.csv.zip"
+)
+IDC_INDEX_PARQUET_FILEPATH: Path | None = _lookup(
+    "idc_index_data/idc_index.parquet", optional=True
+)
