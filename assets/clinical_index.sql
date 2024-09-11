91f6055558e18547c77f3d37c76b8c914b0cdb51
Andrey Fedorov <fedorov@bwh.harvard.edu>
2024-09-11 14:54:53 -0400

    ENH: added clinical_index (#35)
    Originally, this was populated directly from Parquet AWS content in idc-index,
    however, it contains columns that are of no direct use that may confuse the user.
    Further, accessing this index from AWS introduced dependency on the AWS Parquet
    export being up to date. Since this index is quite small, it is best to replace it with a query.

---
+++ assets/clinical_index.sql
@@ -0,0 +1,10 @@
+SELECT
+  collection_id,
+  table_name,
+  SPLIT(table_name,'.')[SAFE_OFFSET(2)] AS short_table_name,
+  `column`,
+  column_label,
+FROM
+  `bigquery-public-data.idc_v19_clinical.column_metadata`
+ORDER BY
+  collection_id, table_name
