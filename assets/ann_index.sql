81cf854ff9f7e67102e9cd812dfeb55c7b384650
Andrey Fedorov <andrey.fedorov@gmail.com>
2026-02-03 16:14:57 -0500

    use ANN SOPClassUID instead of Modality

--- assets/ann_index.sql
+++ assets/ann_index.sql
@@ -20,7 +20,8 @@ WITH
     FROM
       `bigquery-public-data.idc_v23.dicom_all`
     WHERE
-      Modality = "ANN"
+      # Microscopy Bulk Simple Annotations SOP Class UID - more reliable than Modality = "ANN"
+      SOPClassUID = "1.2.840.10008.5.1.4.1.1.91.1"
   ),
 
   -- Unnest AnnotationGroupSequence to get group-level details
