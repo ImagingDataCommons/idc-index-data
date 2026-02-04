4c94905e46ed1ff12e057f89671bdeace50571f9
Andrey Fedorov <andrey.fedorov@gmail.com>
2026-02-04 10:17:17 -0500

    eliminate redundancy, assuming there is one instance per ANN series

--- assets/ann_group_index.sql
+++ assets/ann_group_index.sql
@@ -1,48 +1,15 @@
 # table-description:
 # This table contains detailed metadata about individual annotation groups within
-# Microscopy Bulk Simple Annotations (ANN) instances in IDC. Each row corresponds to
+# Microscopy Bulk Simple Annotations (ANN) series in IDC. Each row corresponds to
 # a single annotation group, providing granular information about the graphic type,
 # number of annotations, property codes, and algorithm details. This table can be
 # joined with ann_index using SeriesInstanceUID for series-level context.
-
-WITH
-  ann_instances AS (
-    SELECT
-      SOPInstanceUID,
-      SeriesInstanceUID,
-      AnnotationCoordinateType,
-      AnnotationGroupSequence,
-      ReferencedSeriesSequence,
-      instance_size,
-      crdc_instance_uuid
-    FROM
-      `bigquery-public-data.idc_v23.dicom_all`
-    WHERE
-      # Microscopy Bulk Simple Annotations SOP Class UID - more reliable than Modality = "ANN"
-      SOPClassUID = "1.2.840.10008.5.1.4.1.1.91.1"
-  ),
-
-  -- Get referenced SOP instance for each ANN instance
-  referenced_instances AS (
-    SELECT
-      ann_instances.SOPInstanceUID AS ann_SOPInstanceUID,
-      ref_instance.ReferencedSOPInstanceUID
-    FROM
-      ann_instances
-    CROSS JOIN
-      UNNEST(ann_instances.ReferencedSeriesSequence) AS ref_series
-    CROSS JOIN
-      UNNEST(ref_series.ReferencedInstanceSequence) AS ref_instance
-  )
+# Note: ANN series are assumed to contain a single instance.
 
 SELECT
   # description:
-  # DICOM SOPInstanceUID identifier of the annotation instance
-  ann_instances.SOPInstanceUID,
-
-  # description:
   # DICOM SeriesInstanceUID for joining with ann_index and idc_index
-  ann_instances.SeriesInstanceUID,
+  ann.SeriesInstanceUID,
 
   # description:
   # sequential number identifying this annotation group as defined in DICOM AnnotationGroupNumber attribute
@@ -69,10 +36,6 @@ SELECT
   group_item.GraphicType,
 
   # description:
-  # coordinate type (2D or 3D) as defined in DICOM AnnotationCoordinateType attribute
-  ann_instances.AnnotationCoordinateType,
-
-  # description:
   # annotation property category code tuple (CodingSchemeDesignator:CodeValue) from DICOM AnnotationPropertyCategoryCodeSequence
   CONCAT(
     group_item.AnnotationPropertyCategoryCodeSequence[SAFE_OFFSET(0)].CodingSchemeDesignator, ":",
@@ -97,38 +60,12 @@ SELECT
   # description:
   # name of the algorithm from DICOM AlgorithmName attribute in AnnotationGroupAlgorithmIdentificationSequence
   # (when AnnotationGroupGenerationType is AUTOMATIC)
-  group_item.AnnotationGroupAlgorithmIdentificationSequence[SAFE_OFFSET(0)].AlgorithmName AS AlgorithmName,
-
-  # description:
-  # SOPInstanceUID of a referenced image instance
-  ANY_VALUE(referenced_instances.ReferencedSOPInstanceUID) AS ReferencedSOPInstanceUID,
-
-  # description:
-  # size of the annotation instance file in bytes
-  ann_instances.instance_size,
-
-  # description:
-  # CRDC UUID for downloading this annotation instance
-  ann_instances.crdc_instance_uuid
+  group_item.AnnotationGroupAlgorithmIdentificationSequence[SAFE_OFFSET(0)].AlgorithmName AS AlgorithmName
 
 FROM
-  ann_instances
+  `bigquery-public-data.idc_v23.dicom_all` AS ann
 CROSS JOIN
-  UNNEST(ann_instances.AnnotationGroupSequence) AS group_item
-LEFT JOIN
-  referenced_instances ON ann_instances.SOPInstanceUID = referenced_instances.ann_SOPInstanceUID
-GROUP BY
-  ann_instances.SOPInstanceUID,
-  ann_instances.SeriesInstanceUID,
-  group_item.AnnotationGroupNumber,
-  group_item.AnnotationGroupUID,
-  group_item.AnnotationGroupLabel,
-  group_item.AnnotationGroupGenerationType,
-  group_item.NumberOfAnnotations,
-  group_item.GraphicType,
-  ann_instances.AnnotationCoordinateType,
-  group_item.AnnotationPropertyCategoryCodeSequence,
-  group_item.AnnotationPropertyTypeCodeSequence,
-  group_item.AnnotationGroupAlgorithmIdentificationSequence,
-  ann_instances.instance_size,
-  ann_instances.crdc_instance_uuid
+  UNNEST(ann.AnnotationGroupSequence) AS group_item
+WHERE
+  # Microscopy Bulk Simple Annotations SOP Class UID - more reliable than Modality = "ANN"
+  SOPClassUID = "1.2.840.10008.5.1.4.1.1.91.1"
